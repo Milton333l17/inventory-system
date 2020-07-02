da3ccac64a86f26da819537a07df0a4bd4bf5080
Stephen Alarcón <slj212885@gmail.com>
2020-07-02 18:41:57 -0500

    Arreglando el diseño de las plantillas

--- login.php
+++ login.php
@@ -2,37 +2,37 @@
 $title = "Login";
 require('layouts/header.php');
 ?>
-<div class="container">
-    <div class="login-wrap">
-        <div class="login-content">
-            <div class="login-logo">
-                <a href="#">
-                    <H1>INVENTORY SYSTEM</H1>
-                </a>
-            </div>
-            <div class="login-form">
-                <form action="" method="post">
-                    <div class="form-group">
-                        <label>Documento</label>
-                        <input class="au-input au-input--full" type="email" name="email" placeholder="Document">
+<div class="page-wrapper">
+    <div class="page-content--bge5">
+        <div class="container">
+            <div class="login-wrap">
+                <div class="login-content">
+                    <div class="login-logo">
+                        <h1>INVENTORY SYSTEM</h1>
                     </div>
-                    <div class="form-group">
-                        <label>Contraseña</label>
-                        <input class="au-input au-input--full" type="password" name="password" placeholder="Password">
+                    <div class="login-form">
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label>Documento</label>
+                                <input class="au-input au-input--full" type="email" name="email" placeholder="Document">
+                            </div>
+                            <div class="form-group">
+                                <label>Contraseña</label>
+                                <input class="au-input au-input--full" type="password" name="password" placeholder="Password">
+                            </div>
+                            <div class="login-checkbox">
+                                <label>
+                                    <input type="checkbox" name="remember">Recordarme
+                                </label>
+                                <label>
+                                    <a href="#">¿Olvidó su contraseña?</a>
+                                </label>
+                            </div>
+                            <button class="au-btn au-btn--block au-btn--blue m-b-20" type="submit">INICIAR SESION</button>
+                        </form>
                     </div>
-                    <div class="login-checkbox">
-                        <label>
-                            <input type="checkbox" name="remember">Recordarme
-                        </label>
-                        <label>
-                            <a href="#">¿Olvidó su contraseña?</a>
-                        </label>
-                    </div>
-                    <button class="au-btn au-btn--block au-btn--blue m-b-20" type="submit">INICIAR SESION</button>
-                </form>
+                </div>
             </div>
         </div>
-    </div>
-</div>
 <?php
 require('layouts/footer.php');
