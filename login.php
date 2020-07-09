279313eddc3808d238b263e71e12980e56d5548b
Milton David <milton333l@hotmail.com>
2020-07-09 18:29:15 -0500

    anexo el login terminado

--- login.php
+++ login.php
@@ -1,69 +1,79 @@
 <?php
 $title = "Login";
 ?>
-  <!-- Fontfaces CSS-->
-  <link href="css/font-face.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
-    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
-
-    <!-- Bootstrap CSS-->
-    <link href="vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
-
-    <!-- Vendor CSS-->
-    <link href="vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
-    <link href="vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
-    <link href="vendor/wow/animate.css" rel="stylesheet" media="all">
-    <link href="vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
-    <link href="vendor/slick/slick.css" rel="stylesheet" media="all">
-    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
-    <link href="vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
-
-    <!-- Main CSS-->
-    <link href="css/theme.css" rel="stylesheet" media="all">
+<!-- Fontfaces CSS-->
+<link href="css/font-face.css" rel="stylesheet" media="all">
+<link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+<link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+<link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
 
 
+<!-- Bootstrap CSS-->
+<link href="vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
 
-<body class="animsition overflow-hidden  bg-dark">
-    <div class="page-wrapper bg-dark ">
-        
-        <!-- MAIN CONTENT-->
-          
-        <div class="container">
-            <div class="login-wrap">
-                <div class="login-content">
-                    <div class="login-logo">
-                        <h1>INVENTORY SYSTEM</h1>
-                    </div>
-                    <div class="login-form ">
-                        <form action="" method="post">
-                            <div class="form-group">
-                                <label>Documento</label>
-                                <input class="au-input au-input--full" type="email" name="email" placeholder="Document">
-                            </div>
-                            <div class="form-group">
-                                <label>Contraseña</label>
-                                <input class="au-input au-input--full" type="password" name="password" placeholder="Password">
-                            </div>
-                            <div class="login-checkbox">
-                                <label>
-                                    <input type="checkbox" name="remember">Recordarme
-                                </label>
-                                <label>
-                                    <a href="#">¿Olvidó su contraseña?</a>
-                                </label>
-                            </div>
-                            <button class="au-btn au-btn--block au-btn--blue m-b-20" type="submit">INICIAR SESION</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-        <div class="mx-auto mt-5 mb-5" style="width: 250px;">
-         <h6 class="text-light">Copyright © 2020 Inventory System.</h6>
-        
+<!-- Vendor CSS-->
+<link href="vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
+<link href="vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
+<link href="vendor/wow/animate.css" rel="stylesheet" media="all">
+<link href="vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
+<link href="vendor/slick/slick.css" rel="stylesheet" media="all">
+<link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+<link href="vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
+<!-- Main CSS-->
+<!-- <link href="css/theme.css" rel="stylesheet" media="all"> -->
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
+  <link rel="stylesheet" href="css/estilo_login.css">
+</head>
+<body>
+  <form action="#" class="login-form">
+    <h1>Inventory System</h1>
+    <div class="textb">
+      <input type="text" required>
+      <div class="placeholder">Documento</div>
     </div>
+
+    <div class="textb">
+      <input type="password" required>
+      <div class="placeholder">Contraseña</div>
+      <div class="show-password fas fa-eye-slash"></div>
+    </div>
+
+    <button class="btn fas fa-arrow-right" ></button>
+    <p class="text-center">Copyright © 2020 Inventory System.</p>
+  </form>
+
+  <script>
+    var fields = document.querySelectorAll(".textb input");
+    var btn = document.querySelector(".btn");
+    function check(){
+      if(fields[0].value != "" && fields[1].value != "")
+        btn.disabled = false;
+      else
+        btn.disabled = true;  
+    }
+
+    fields[0].addEventListener("keyup",check);
+    fields[1].addEventListener("keyup",check);
+
+    document.querySelector(".show-password").addEventListener("click",function(){
+      if(this.classList[2] == "fa-eye-slash"){
+        this.classList.remove("fa-eye-slash");
+        this.classList.add("fa-eye");
+        fields[1].type = "text";
+      }else{
+        this.classList.remove("fa-eye");
+        this.classList.add("fa-eye-slash");
+        fields[1].type = "password";
+      }
+    });
+  </script>
+</body>
+
 
 <!-- Jquery JS-->
 <script src="vendor/jquery-3.2.1.min.js"></script>
