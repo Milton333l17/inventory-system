9ea739976dddd8314d15cb135ccf0734505de563
Milton David <milton333l@hotmail.com>
2020-10-19 14:13:44 -0500

    agregando nuevas tablas a la bd

--- inventorysystem.sql
+++ inventorysystem.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-08-2020 a las 21:21:47
--- Versión del servidor: 10.1.38-MariaDB
--- Versión de PHP: 7.3.2
+-- Tiempo de generación: 19-10-2020 a las 21:09:52
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,6 +20,18 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `inventorysystem`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `estado` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -53,6 +64,15 @@ CREATE TABLE `estado` (
   `tipo_estado` tinytext COLLATE utf8_spanish_ci NOT NULL COMMENT 'se sellecciona si llego en buen o mal estado'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `estado`
+--
+
+INSERT INTO `estado` (`idestado`, `tipo_estado`) VALUES
+(1, 'obsoleto'),
+(2, 'dañado'),
+(3, 'activo');
+
 -- --------------------------------------------------------
 
 --
@@ -66,7 +86,7 @@ CREATE TABLE `login_usuario` (
   `apellidos` varchar(45) COLLATE utf8_spanish_ci NOT NULL COMMENT 'aqui se guardara los apellidos de la persona',
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT 'aqui se guardara la contraseña asignada',
   `email` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
-  `estado` int(11) NOT NULL DEFAULT '0',
+  `estado` int(11) NOT NULL DEFAULT 0,
   `tip_doc_id` int(11) NOT NULL COMMENT 'aqui se guardara la ide del tipo del documento',
   `rol_id` int(11) NOT NULL COMMENT 'en este campo la persona seleccionara el rol a la que pertenece la persona'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
@@ -76,7 +96,24 @@ CREATE TABLE `login_usuario` (
 --
 
 INSERT INTO `login_usuario` (`id`, `documento`, `nombres`, `apellidos`, `password`, `email`, `estado`, `tip_doc_id`, `rol_id`) VALUES
-(1, 1001995096, 'Stephen', 'Alarcon', '$2y$10$cXajR4X9FoEu/ZSp0QuJW.hPbBoyZmWS65QXbI7At/UklWk4pbDcS', 'stephen@gmail.com', 0, 1, 1);
+(1, 1001995096, 'Stephen', 'Alarcon', '$2y$10$cXajR4X9FoEu/ZSp0QuJW.hPbBoyZmWS65QXbI7At/UklWk4pbDcS', 'stephen@gmail.com', 0, 1, 1),
+(20, 1006656642, 'milton', 'araque', '$2y$10$bLf35K3URKfc7xdl28AryenhuvleONdnSPUlsAJz0D6ePQU9WUKb.', 'milton333l@hotmail.com', 1, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `descripcion` varchar(95) COLLATE utf8_unicode_ci NOT NULL,
+  `unidad_medida_id` int(11) NOT NULL,
+  `categoria_id` int(11) NOT NULL,
+  `estado` tinyint(4) NOT NULL,
+  `provedor_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -85,7 +122,7 @@ INSERT INTO `login_usuario` (`id`, `documento`, `nombres`, `apellidos`, `passwor
 --
 
 CREATE TABLE `provedor` (
-  `idprovedor` varchar(20) COLLATE utf8_spanish_ci NOT NULL COMMENT 'se guarda la id del provedor',
+  `idprovedor` int(20) NOT NULL COMMENT 'se guarda la id del provedor',
   `telefono` int(11) NOT NULL COMMENT 'se guarda el telefono del provedor',
   `nombre` int(11) NOT NULL COMMENT 'se guarda el nombre de la persona o el laboratorio(provedor)',
   `direccion` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL COMMENT 'se guarda la dirreccion del provedor'
@@ -170,6 +207,13 @@ CREATE TABLE `unidad_medida` (
 --
 
 --
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
 -- Indices de la tabla `entradas`
 --
 ALTER TABLE `entradas`
@@ -180,6 +224,12 @@ ALTER TABLE `entradas`
   ADD KEY `tipo_producto_id` (`tipo_producto_id`);
 
 --
+-- Indices de la tabla `estado`
+--
+ALTER TABLE `estado`
+  ADD PRIMARY KEY (`idestado`);
+
+--
 -- Indices de la tabla `login_usuario`
 --
 ALTER TABLE `login_usuario`
@@ -188,6 +238,22 @@ ALTER TABLE `login_usuario`
   ADD UNIQUE KEY `email` (`email`),
   ADD KEY `tip_doc_id` (`tip_doc_id`),
   ADD KEY `rol_id` (`rol_id`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`),
+  ADD KEY `unidad_medida_id` (`unidad_medida_id`),
+  ADD KEY `categoria_id` (`categoria_id`),
+  ADD KEY `provedor_id` (`provedor_id`);
+
+--
+-- Indices de la tabla `provedor`
+--
+ALTER TABLE `provedor`
+  ADD PRIMARY KEY (`idprovedor`);
 
 --
 -- Indices de la tabla `rol`
@@ -204,14 +270,50 @@ ALTER TABLE `tip_doc`
   ADD UNIQUE KEY `descripcion` (`descripcion`);
 
 --
+-- Indices de la tabla `unidad_medida`
+--
+ALTER TABLE `unidad_medida`
+  ADD PRIMARY KEY (`idunidad_medida`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `estado`
+--
+ALTER TABLE `estado`
+  MODIFY `idestado` int(11) NOT NULL AUTO_INCREMENT COMMENT 'se guarda la id del estado el producto', AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `login_usuario`
 --
 ALTER TABLE `login_usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `provedor`
+--
+ALTER TABLE `provedor`
+  MODIFY `idprovedor` int(20) NOT NULL AUTO_INCREMENT COMMENT 'se guarda la id del provedor';
+
+--
+-- AUTO_INCREMENT de la tabla `unidad_medida`
+--
+ALTER TABLE `unidad_medida`
+  MODIFY `idunidad_medida` int(11) NOT NULL AUTO_INCREMENT COMMENT 'se guarda la id del la unidad que se va utilizar';
 
 --
 -- Restricciones para tablas volcadas
@@ -223,6 +325,14 @@ ALTER TABLE `login_usuario`
 ALTER TABLE `login_usuario`
   ADD CONSTRAINT `login_usuario_ibfk_1` FOREIGN KEY (`rol_id`) REFERENCES `rol` (`idRol`),
   ADD CONSTRAINT `login_usuario_ibfk_2` FOREIGN KEY (`tip_doc_id`) REFERENCES `tip_doc` (`idTip_doc`);
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `productos_ibfk_2` FOREIGN KEY (`provedor_id`) REFERENCES `provedor` (`idprovedor`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `productos_ibfk_3` FOREIGN KEY (`unidad_medida_id`) REFERENCES `unidad_medida` (`idunidad_medida`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
